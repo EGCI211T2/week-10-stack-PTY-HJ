79e9dbf0c92b1c031f9671ba8bd357555676ecda
PTY-HJ <pth.honey29@gmail.com>
2025-11-18 03:37:23 +0000

    18th

--- stack.h
+++ stack.h
@@ -16,11 +16,16 @@ public:
 void Stack::push(int x){
   NodePtr new_node=new NODE(x);
   if(new_node){
-    
-            // Left missing for exercises…
+     //connect the previous top
+    new_node->set_next(top);
+    //change top
+    top=new_node;
+    //change size
+    size++;
    }
- 
-         // Left missing for exercises…
+   else{
+    cout<<"Not enough memory"<<endl;
+   }
     
 }
 
@@ -29,23 +34,32 @@ int Stack::pop(){
         int value;
         if(t!=NULL){
             value=t->get_value();
-        // Left missing part for exercises
         // move top to the next one
+            top=t->get_next();
         // decrease the size
+            --size;
             delete t;
             return value;
         }
-	//be careful of the empty stack!!!
+        //be careful of the empty stack!!!
+        cout<<"Empty stack"<<endl;
+         return 0;
     }
 
 Stack::Stack(){
     //initialize stack
+    top=NULL;
+    size=0;
     
 }
 Stack::~Stack(){
-    //delete all remaning stack (i.e. pop all) 
-    
+    //delete all remaning stack (i.e. pop all)
+    cout<<"Clear the stack"<<endl;
+    int n=size;
+    while(n>0){
+        pop();
+        n--;
+    }
 }
-
 
 #endif
