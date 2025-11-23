c58d1d2d1b2cdb9ae1753dd7f77feb2457db4410
PTY-HJ <pth.honey29@gmail.com>
2025-11-23 06:29:10 +0000

    23rd

--- main2.cpp
+++ main2.cpp
@@ -5,25 +5,47 @@ using namespace std;
 
 int main(int argc, char **argv){
     Stack s;
+    int i,j;
    
-/*
-  Exercise 2
- printf("Checking the parentheses in argv arguments\n");
+    cout<<"Checking the parenthesis in argv argunments"<<endl;
 
-   */
+    for(i=1;i<argc;i++){
+    int correct=1;
 
-    /*
-     for(j=0;j<strlen(argv[i]);j++){
-       // Use stack to help with the parentheses
+      for(j=0;j<strlen(argv[i]);j++){
 
+        if(argv[i][j]=='['||argv[i][j]=='{'){
+          s.push(argv[i][j]);
+        }
 
+        else{
+          if(argv[i][j]==']'||argv[i][j]=='}'){
+            //if the stack is empty, it means there is a closing with no opening
+            if(s.isEmpty()){
+              correct=0;
+              break;
+            }
 
+            char x=s.pop();
 
+            //check whether the type matches or not for the closing and opening
+            if ((argv[i][j] == ']' && x != '[') || (argv[i][j] == '}' && x != '{')) {
+            correct = 0;
+            break;
+          }
+        }
+      }
+    }
+    if(!s.isEmpty()){
+      correct=0;
+      cout<<"Too many opening brackets left"<<endl;
+    }
+
+    if (correct == 1)
+        cout << "argv " << i << " correct" << endl;
+    else
+        cout << "argv " << i << " incorrect" << endl;
 
   }
-
-  */
-
-
    return 0;
 }
